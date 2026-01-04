0f7ac2d1ab2c0ed7b8d85d8e70715d9fec63a577
HARSHTRIPATHI18 <102883937+HARSHTRIPATHI18@users.noreply.github.com>
2026-01-04 20:29:48 +0530

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_dynamodb_table" "events" {
+  name         = "events"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "event_id"
+
+  attribute {
+    name = "event_id"
+    type = "S"
+  }
+}
